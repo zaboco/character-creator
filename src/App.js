3167e9da5a0f8ff6bb40b3f40ea79315a55a9e7a
Bogdan Zaharia <bogdan.zaharia@hootsuite.com>
2022-03-11 19:08:49 +0200

    add background decoration

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
+import BackgroundDecoration from './components/BackgroundDecoration/BackgroundDecoration.js';
 
 function App() {
   return (
     <>
+      <BackgroundDecoration />
       <CharacterEditor />
       <Footer />
     </>
---
+++ src/components/BackgroundDecoration/BackgroundDecoration.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './BackgroundDecoration.module.css';
+
+const BackgroundDecoration = () => {
+  return (
+    <div className={styles.decoration}/>
+  );
+};
+
+export default BackgroundDecoration;
